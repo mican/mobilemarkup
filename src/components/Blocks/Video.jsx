6b944751d84812b2f34cf96fdd325b1462a0b57c
Michał Niewitała <niewitala@lightweb.pl>
2024-05-15 20:02:26 +0200

    Update Video.jsx

--- src/components/Blocks/Video.jsx
+++ src/components/Blocks/Video.jsx
@@ -1,11 +1,52 @@
-import React, { Suspense } from 'react'
-
-const LazyYoutube = React.lazy(() => import('./Youtube.jsx'))
+import React, { useRef, useEffect, useState } from 'react'
 
 export default function Video({ id }) {
+  const videoRef = useRef(null)
+  const [isVisible, setIsVisible] = useState(false)
+  const posterSrc = `https://img.youtube.com/vi/${id}/maxresdefault.jpg`
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1.0 // Fully visible
+    }
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio === 1) {
+          setIsVisible(true)
+          observer.unobserve(entry.target)
+        }
+      })
+    }, options)
+
+    if (videoRef.current) {
+      observer.observe(videoRef.current)
+    }
+
+    return () => {
+      if (videoRef.current) {
+        observer.unobserve(videoRef.current)
+      }
+    }
+  }, [])
+
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <LazyYoutube id={id} />
-    </Suspense>
+    <div>
+      {isVisible ? (
+        <iframe
+          ref={videoRef}
+          className="w-full aspect-video pointer-events-none"
+          src={'https://www.youtube.com/embed/' + id + '?controls=0&autoplay=1&mute=1&loop=1&playlist=' + id}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      ) : (
+        <img src={posterSrc} alt="Poster" />
+      )}
+    </div>
   )
 }
