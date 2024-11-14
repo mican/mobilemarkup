ac375a07309f263e81145c342b943d410afb64f1
Michał Niewitała <niewitala@lightweb.pl>
2024-11-14 18:43:18 +0100

    Update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mobilemarkup.com',
   integrations: [tailwind(), react(), sitemap()],
   vite: {
     css: {
