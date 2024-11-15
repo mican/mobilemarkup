c1166a5337b978cf20893a0eea0dc88607f5f36d
Michał Niewitała <niewitala@lightweb.pl>
2024-11-15 10:34:40 +0100

    Astro matomo again

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,20 @@ import react from '@astrojs/react'
 
 import sitemap from '@astrojs/sitemap'
 
+import matomo from '@jop-software/astro-matomo'
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mobilemarkup.com',
-  integrations: [tailwind(), react(), sitemap()],
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap(),
+    matomo({
+      baseUrl: 'https://lightweb.matomo.cloud/',
+      siteId: 2
+    })
+  ],
   vite: {
     css: {
       modules: {
