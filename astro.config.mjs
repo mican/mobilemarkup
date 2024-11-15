56b5f2586ee5130521f2932b3e7c1ef052798d03
Michał Niewitała <niewitala@lightweb.pl>
2024-11-15 09:51:53 +0100

    Matomo stats

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,26 @@ import react from '@astrojs/react'
 
 import sitemap from '@astrojs/sitemap'
 
+import matomo from 'astro-matomo'
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mobilemarkup.com',
-  integrations: [tailwind(), react(), sitemap()],
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap(),
+    matomo({
+      enabled: import.meta.env.PROD, // Only load in production
+      host: 'https://lightweb.matomo.cloud/',
+      trackerUrl: 'js/', // defaults to matomo.php
+      srcUrl: 'js/', // defaults to matomo.js
+      siteId: 2,
+      heartBeatTimer: 5,
+      disableCookies: true,
+      debug: true
+    })
+  ],
   vite: {
     css: {
       modules: {
